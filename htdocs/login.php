48862bfe9d2d225e49b772f4a7c5ddab26b4eb42
GLADOS <dmsgur5604@gmail.com>
2024-06-18 13:32:28 +0900

    Add debugging messages to login.php

--- htdocs/login.php
+++ htdocs/login.php
@@ -1,3 +1,37 @@
+<?php
+include 'db.php';
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    echo "Username: $username<br>";
+    echo "Password: $password<br>";
+
+    $sql = "SELECT * FROM users WHERE username='$username'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        echo "User found: " . $row['username'] . "<br>";
+
+        if (password_verify($password, $row['password'])) {
+            $_SESSION['username'] = $username;
+            echo "Login successful<br>";
+            header("Location: welcome.php");
+            exit();
+        } else {
+            echo "Invalid password<br>";
+        }
+    } else {
+        echo "No user found<br>";
+    }
+} else {
+    echo "No POST data received.<br>";
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
