cf75fd3ff470fa8018252f38c198bf9cdf4b732f
GLADOS <dmsgur5604@gmail.com>
2024-06-18 13:43:21 +0900

    Add index.php and update project structure

--- htdocs/header.php
+++ htdocs/header.php
@@ -5,13 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Your Website</title>
-    <link rel="stylesheet" href="/htdocs/css/styles.css"> <!-- 절대 경로로 수정 -->
-    <script src="/htdocs/js/scripts.js" defer></script> <!-- 절대 경로로 수정 -->
+    <link rel="stylesheet" href="/pythonProject/htdocs/css/styles.css"> <!-- 상대 경로로 수정 -->
+    <script src="/pythonProject/htdocs/js/scripts.js" defer></script> <!-- 상대 경로로 수정 -->
 </head>
 <body>
     <nav>
-        <a href="/htdocs/index.php">Home</a>
-        <a href="/htdocs/login.php">Login</a>
-        <a href="/htdocs/register.php">Register</a>
+        <a href="/pythonProject/htdocs/index.php">Home</a>
+        <a href="/pythonProject/htdocs/login.php">Login</a>
+        <a href="/pythonProject/htdocs/register.php">Register</a>
         <!-- Add other navigation links as needed -->
     </nav>
