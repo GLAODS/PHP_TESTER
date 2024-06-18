43551ba42e8004b74bc5919ecc2fccd1606701e6
GLADOS <dmsgur5604@gmail.com>
2024-06-18 11:21:30 +0900

    Add index.php and update project structure

--- htdocs/db.php
+++ htdocs/db.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 
 $servername = "sql212.infinityfree.com";
 $username = "if0_36742561";
-$password = "Your vPanel Password";
+$password = "j2PIb1M59SSh"; // 올바른 비밀번호 입력
 $dbname = "if0_36742561_TEST_DB";
 
 // Create connection
