82267a8d352fea64397ee94f58489d681cfde9f7
GLADOS <dmsgur5604@gmail.com>
2024-06-18 11:17:42 +0900

    Add index.php and update project structure

--- htdocs/db.php
+++ htdocs/db.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $servername = "sql212.infinityfree.com";
 $username = "if0_36742561";
 $password = "Your vPanel Password";
--- htdocs/welcome.php
+++ htdocs/welcome.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 
 if (!isset($_SESSION['username'])) {
@@ -11,11 +15,17 @@ echo "Welcome, " . $_SESSION['username'] . "!";
 
 <!DOCTYPE html>
 <html>
+<head>
+    <title>Welcome</title>
+</head>
 <body>
-
-<h2>Welcome Page</h2>
-<p>You have successfully logged in.</p>
-<a href="logout.php">Logout</a>
-
+    <nav>
+        <a href="index.php">Home</a> |
+        <a href="register.php">Register</a> |
+        <a href="login.php">Login</a> |
+        <a href="logout.php">Logout</a>
+    </nav>
+    <h2>Welcome Page</h2>
+    <p>You have successfully logged in.</p>
 </body>
 </html>
